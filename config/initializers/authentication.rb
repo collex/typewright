6ab446b3679295a75d6af437ac47782412763f7b
Lou Foster <lou@performantsoftware.com>
2015-05-21 14:36:37 -0400

    possible fix for symbols in xml

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -2,3 +2,5 @@ PRIVATE_TOKEN = SITE_SPECIFIC['authentication']['private_token']
 EMOP_TOKEN = SITE_SPECIFIC['authentication']['emop_token']
 EMOP_URL = SITE_SPECIFIC['authentication']['emop_root_url']
    
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+   
